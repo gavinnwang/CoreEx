46a903c42964d61b5776c692e86f804029c87433
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-08-08 16:09:24 +0800

    progress

--- orderbook_test.go
+++ orderbook_test.go
@@ -2,17 +2,38 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
+func assert(t *testing.T, a, b any) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("%+v != %+v", a, b)
+	}
+}
 func TestLimit(t *testing.T) {
 	l := NewLimit(10_000)
-	buyOrder := NewOrder(true, 5)
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 5)
+	buyOrderC := NewOrder(true, 7.5)
+	l.AddOrder(buyOrderA)
+	l.AddOrder(buyOrderB)
+	l.AddOrder(buyOrderC)
 
-	l.AddOrder(buyOrder)
+	l.DeleteOrder(buyOrderB)
 
 	fmt.Println(l)
 }
-func TestOrderbook(t *testing.T) {
-	
+
+func TestPlaceLimitOrder(t *testing.T) {
+	ob := NewOrderbook()
+
+	sellOrderA := NewOrder(false, 10)
+	sellOrderB := NewOrder(false, 5)
+	ob.PlaceLimitOrder(10_000, sellOrderA)
+	ob.PlaceLimitOrder(9_000, sellOrderB)
+
+	assert(t, len(ob.asks), 2)
+	assert(t, len(ob.bids), 0)
+
 }
